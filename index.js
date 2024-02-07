7e8854444e9fe4d40b3d5a2036ed20b21c93234a
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-02-07 16:18:17 +0200

    ejs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Revison');
+// common js but the package.json must not include "type": "module"
+// const {addition} = require('./addition')
+import {addition} from './addition.js'
+import {subtraction} from './subtraction.js'
+
+console.log(addition(3, 6));
+console.log(subtraction(7,3));
+console.log(subtraction(3,7));
